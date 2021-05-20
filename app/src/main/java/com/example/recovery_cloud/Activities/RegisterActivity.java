002db70ed1e2f4790583387cb327118a9be5d4e3
Nick Kun <nikhilk21234@gmail.com>
2021-05-20 10:48:39 +0530

    Registeration And Login Activities

--- app/src/main/java/com/example/recovery_cloud/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/recovery_cloud/Activities/RegisterActivity.java
@@ -1,16 +1,147 @@
 package com.example.recovery_cloud.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.recovery_cloud.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userEmail,userPassword,userPAssword2,userName;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //ini views
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPassword);
+        userPAssword2 = findViewById(R.id.regPassword2);
+        userName = findViewById(R.id.regName);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+        loadingProgress.setVisibility(View.INVISIBLE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPAssword2.getText().toString();
+                final String name = userName.getText().toString();
+
+                if( email.isEmpty() || name.isEmpty() || password.isEmpty()  || !password.equals(password2)) {
+
+                    showMessage("Please Verify all fields") ;
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+
+                }
+
+                else {
+
+                    CreateUserAccount(email,name,password);
+
+                }
+
+            }
+
+        });
     }
+
+
+    private void CreateUserAccount(String email, final String name, String password) {
+
+
+        // this method create user account with specific email and password
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    showMessage("Account Created");
+                    updateUserInfo(name, mAuth.getCurrentUser());
+
+                }
+                else {
+
+                    showMessage("Account creation failed... " + task.getException().getMessage());
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+
+                }
+
+            }
+
+        });
+
+    }
+
+    private void updateUserInfo(final String name, final FirebaseUser currentUser) {
+
+        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+        currentUser.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()) {
+
+                    showMessage("Registration Complete");
+                    updateUI();
+
+                }
+
+            }
+
+        });
+
+
+    }
+
+    private void updateUI() {
+
+        Intent homeActivity = new Intent(getApplicationContext(),HomeActivity.class);
+        startActivity(homeActivity);
+        finish();
+
+    }
+
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+
+    }
+
 }
